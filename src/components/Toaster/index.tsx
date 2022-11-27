13dbdf710813bc2f8c512ad681186a7f6cb3068a
d3vlimpiado <jianlimpiado@gmail.com>
2022-11-27 22:27:56 +0800

    underdevelopment: toaster; v1

--- src/components/Toaster/index.tsx
+++ src/components/Toaster/index.tsx
@@ -1,17 +1,40 @@
+import React from "react";
 import style from "./toaster.module.css";
 
 type ToasterProps = {
-  message?: string;
+  message?: string|"Toaster by Josh";
+  duration?: number|1000;
 }
 
-// export function toast(){
+const ToasterContext = React.createContext({} as ToasterProps);
+const ToasterUpdateContext = React.createContext((obj: ToasterProps) => {});
 
-// }
+export function toast(){
 
-function Toaster({message = "Personal Toaster by Josh"}: ToasterProps){
+}
+
+function Toast(){
   return <div className={style.main}>
-    <p>{message}</p>
+    <p>{"message"}</p>
   </div>
+}
+
+type ToastProviderProps = {
+  children: React.ReactNode;
+}
+
+function Toaster({children}:ToastProviderProps){
+  const [toastProps, setToastProps] = React.useState({} as ToasterProps);
+
+  const handleUpdate = (obj: ToasterProps) =>{
+    setToastProps(prev => prev = obj);
+  }
+
+  return <ToasterContext.Provider value={toastProps}>
+    <ToasterUpdateContext.Provider value={handleUpdate}>
+      {children}
+    </ToasterUpdateContext.Provider>
+  </ToasterContext.Provider>
 }
 
 export default Toaster;
